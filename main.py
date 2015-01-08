d442dca35773c03b55f0026f4773d72169ad27d3
seansmccullough <seansmccullough@gmail.com>
2015-01-08 13:32:58 -0800

    commented main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hello, world")
+
+#balance check
+
+#latency check
+
+#wait until designated time - return of latency check
+
+#submit new loan list request
+
+#filter new loans
+
+#submit loan buy request
